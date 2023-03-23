cbe52c8ab1fcf4ae37e9a16f695424659ffdbd20
Sebastien <seb_bielecki@hotmail.com>
2023-03-23 17:31:46 -0600

    Feat: React Test setup

---
+++ src/Alert.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Alert = () => {
+
+  return <>
+    <h2>Alert Component</h2>
+  </>
+}
+
+export default Alert
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+
+function App() {
+  
+
+  return <>
+    <section className='section-center'>
+      <Alert></Alert>
+      <List></List>
+    </section>
+  </>
+}
+
+export default App
